ed5eab4f81e2d2a70ddfb9a822524288cc97439b
ziggynyathi <ziggynyathi091@gmail.com>
2023-06-02 14:48:59 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+        <div  className='row1' >
+        
+          <div className='flexbox-item-1'>item1</div>
+          <div className='flexbox-item-2'>item2</div>
+          <div className='flexbox-item-3'>item3</div>
+        </div>
+
+      
+        <div  className='row2' >
+        
+          <div className='flexbox-item-4'>item4</div>
+          <div className='flexbox-item-5'>item5</div>
+          <div className='flexbox-item-6'>item6</div>
+        </div>
+        <div>
+        <div className='flexbox-container2'>container</div>
+        </div>
+    
+
+        
+       
+    
+      
+    
+      
+    </div>
+  );
+}
+
+export default App;
